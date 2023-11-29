d346f0af77d25697d4675970463c8617a62905f5
ilasutoriasu <vaioretoriri@gmail.com>
2023-11-29 13:20:53 +0900

    ノルンが増殖するバグの修正

--- Assets/Script/DontDestroy.cs
+++ Assets/Script/DontDestroy.cs
@@ -14,6 +14,10 @@ public class DontDestroy : MonoBehaviour
             instance = this;
             DontDestroyOnLoad(gameObject);
         }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
     // Start is called before the first frame update
     void Start()
