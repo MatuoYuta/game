880e795af961190bddfd392615a07312ddf7c864
matuo2525 <yu-.55ta@docomo.ne.jp>
2023-12-04 11:59:31 +0900

    時計の調整

--- Assets/GMsozai/tokei/hourHandManager.cs
+++ Assets/GMsozai/tokei/hourHandManager.cs
@@ -1,22 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using System;
+using UnityEngine;
 
-public class hourHandManager : MonoBehaviour
+public class hourHandsManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    // 自分で指定する中心となる時間（24時間形式）
+    public int startHour = 12;
+    public int startMinute = 0;
 
+    // 1秒あたりの仮想時間の進行速度
+    public float timeSpeed = 1.0f;
+
+    private void Update()
+    {
+        // 現在の時間と指定した中心時間の差分を計算
+        TimeSpan timeDifference = DateTime.Now.TimeOfDay - new TimeSpan(startHour, startMinute, 0);
+
+        // 指定した中心時間からの経過時間に応じて時計の針を更新
+        UpdateClockHands(timeDifference);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdateClockHands(TimeSpan elapsedTime)
     {
-        int m = DateTime.Now.Minute;
-        int h = DateTime.Now.Hour;
-        float HHA = -((360 / 12.0f * h) + (30 / 60.0f * m));
-        GetComponent<Transform>().localEulerAngles = new Vector3(0, 0, HHA);
+        // 時針の角度を計算
+        float hourHandAngle = -((360 / 12.0f) * ((float)elapsedTime.TotalHours % 12));
+
+        // 分針の角度を計算
+        float minuteHandAngle = -((360 / 60.0f) * ((float)elapsedTime.TotalMinutes % 60));
+
+        // 時計の針の角度を更新
+        transform.Find("hourHand").localEulerAngles = new Vector3(0, 0, hourHandAngle);
+        transform.Find("minuteHand").localEulerAngles = new Vector3(0, 0, minuteHandAngle);
     }
 }
+
+
