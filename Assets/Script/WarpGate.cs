24bc2694c90a451b032e1e060e4cfb0120e968c3
ilasutoriasu <vaioretoriri@gmail.com>
2023-12-05 10:17:38 +0900

    背景アセットの追加

--- Assets/Script/WarpGate.cs
+++ Assets/Script/WarpGate.cs
@@ -5,18 +5,73 @@ public class WarpGate : MonoBehaviour
     // ワープ先の位置
     public Transform warpDestination;
 
-    private void OnTriggerEnter2D(Collider2D other)
+    // ワープを実行するキー
+    public KeyCode warpKey = KeyCode.W;
+
+    // ワープを実行する対象のオブジェクト
+    public GameObject targetObject;
+
+    private void Update()
     {
-        if (other.CompareTag("Player"))
+        // キーが押されたかどうかを確認
+        if (Input.GetKeyDown(warpKey))
         {
-            WarpPlayer(other.transform);
+            // ターゲットオブジェクトの前でキーが押されたかどうかを確認
+            if (IsPlayerInFrontOfGate())
+            {
+                // ワープを実行
+                WarpPlayer();
+            }
         }
     }
 
-    private void WarpPlayer(Transform playerTransform)
+    private bool IsPlayerInFrontOfGate()
     {
-        Debug.Log("わーぴ");
+        if (targetObject == null)
+        {
+            Debug.LogWarning("Target object not set");
+            return false;
+        }
+
+        // ターゲットオブジェクトの前にプレイヤーがいるかどうかを確認
+        Collider2D targetCollider = targetObject.GetComponent<Collider2D>();
+        if (targetCollider == null)
+        {
+            Debug.LogWarning("Target object does not have a Collider2D component");
+            return false;
+        }
+
+        Collider2D playerCollider = GameObject.FindGameObjectWithTag("Player")?.GetComponent<Collider2D>();
+        if (playerCollider == null)
+        {
+            Debug.LogWarning("Player object not found");
+            return false;
+        }
+
+        return Physics2D.IsTouching(targetCollider, playerCollider);
+    }
+
+    private void WarpPlayer()
+    {
         // プレイヤーをワープ先の位置に移動させる
-        playerTransform.position = warpDestination.position;
+        if (warpDestination != null)
+        {
+            GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+            if (playerObject != null)
+            {
+                Transform playerTransform = playerObject.transform;
+                playerTransform.position = warpDestination.position;
+                Debug.Log("Player warped");
+            }
+            else
+            {
+                Debug.LogWarning("Player not found");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Warp destination not set");
+        }
     }
 }
+
