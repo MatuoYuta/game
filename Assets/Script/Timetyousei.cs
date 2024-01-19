814d45870d4bb09c482a5456c9962cb9f4c46ef9
matuo2525 <yu-.55ta@docomo.ne.jp>
2024-01-19 09:57:39 +0900

    メニューの調整と時計の調整

--- Assets/Script/Timetyousei.cs
+++ Assets/Script/Timetyousei.cs
@@ -9,39 +9,51 @@ public class Timetyousei : MonoBehaviour
 
     bool menuzyoutai;
 
+    void Start()
+    {
+        MenuObject2.SetActive(false);
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.G))
         {
             メニューの切り替え();
+            Debug.Log(menuzyoutai);
+           
         }
     }
 
     void メニューの切り替え()
     {
-        menuzyoutai = !menuzyoutai;
+        //menuzyoutai = !menuzyoutai;
 
-        if (menuzyoutai)
+        if (menuzyoutai == true)
         {
+
             // メニューを表示
             MenuObject.SetActive(true);
 
             MenuObject2.SetActive(false);
+
+            menuzyoutai = false;
             // マウスカーソルを表示にし、位置固定解除
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
         }
         else
         {
+
+            menuzyoutai = true;
             // メニューを非表示
             MenuObject.SetActive(false);
 
             MenuObject2.SetActive(true);
 
             // マウスカーソルを非表示にし、位置を固定
-            Cursor.visible = false;
-            Cursor.lockState = CursorLockMode.Locked;
+            /*Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;*/
         }
     }
 }
