c4e5dad0ec94e5cc0612d3acd17557af035fcc29
matuo2525 <yu-.55ta@docomo.ne.jp>
2024-01-19 11:38:14 +0900

    カメラの位置調整

--- Assets/Script/Timetyousei.cs
+++ Assets/Script/Timetyousei.cs
@@ -6,12 +6,14 @@ public class Timetyousei : MonoBehaviour
 {
     [SerializeField] GameObject MenuObject;
     [SerializeField] GameObject MenuObject2;
+    [SerializeField] GameObject MenuObject3;
 
     bool menuzyoutai;
 
     void Start()
     {
         MenuObject2.SetActive(false);
+        MenuObject3.SetActive(false);
     }
 
     // Update is called once per frame
@@ -42,6 +44,8 @@ public class Timetyousei : MonoBehaviour
 
             MenuObject2.SetActive(false);
 
+            MenuObject3.SetActive(false);
+
             menuzyoutai = false;
             // マウスカーソルを表示にし、位置固定解除
             Cursor.visible = true;
@@ -58,6 +62,8 @@ public class Timetyousei : MonoBehaviour
             MenuObject.SetActive(false);
 
             MenuObject2.SetActive(true);
+
+            MenuObject3.SetActive(true);
 
             // マウスカーソルを非表示にし、位置を固定
             /*Cursor.visible = false;
