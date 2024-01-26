ab3ab8b520824f1adf01b182114b575128b430f1
matuo2525 <yu-.55ta@docomo.ne.jp>
2024-01-26 15:52:23 +0900

    ステージ01制作

--- Assets/Script/Gimmick/GimmickWall/Door.cs
+++ Assets/Script/Gimmick/GimmickWall/Door.cs
@@ -3,20 +3,32 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     private Animator anim1;
+    private Rigidbody2D rigid;
+    private BoxCollider2D col;
+
+    float x;
+    float y;
+    private void Start()
+    {
+        x = this.gameObject.transform.position.x;
+        y = this.gameObject.transform.position.y;
+    }
 
     private void Awake()
     {
-        anim1 = GetComponent<Animator>();
-        anim1.SetBool("isOpen", false);
+        //anim1 = GetComponent<Animator>();
+        //anim1.SetBool("isOpen", false);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (anim1.enabled && collision.GetComponent<Player>().HasKey == true)
+        if (collision.GetComponent<Player>().HasKey == true)
         {
             // ドアを開く処理をここに追加
-            anim1.SetBool("isOpen", true);
+            //anim1.SetBool("isOpen", true);
+            this.gameObject.transform.position = new Vector2(x, y * 10);
             Debug.Log("Door opened!");
             collision.GetComponent<Player>().HasKey = false;
+
         } else
         {
             Debug.Log("カギがないよ");
