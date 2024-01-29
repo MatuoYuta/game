b6bda46d0b202d63ffa0e93cc76c4c226c32e542
matuo2525 <yu-.55ta@docomo.ne.jp>
2024-01-29 10:24:14 +0900

    背景の完成

--- Assets/Script/BackGround/BackGround.cs
+++ Assets/Script/BackGround/BackGround.cs
@@ -10,12 +10,16 @@ public class BackGround : MonoBehaviour
 
     private GameObject _camera;
     private float _length;
+    private float _lengthY;
     private float _startPosX;
+    private float _startPosY;
     // Start is called before the first frame update
     void Start()
     {
         _startPosX = transform.position.x;
+        _startPosY = transform.position.y;
         _length = GetComponent<SpriteRenderer>().bounds.size.x;
+        _lengthY = GetComponent<SpriteRenderer>().bounds.size.y;
         _camera = Camera.main.gameObject;
     }
 
@@ -27,6 +31,7 @@ public class BackGround : MonoBehaviour
     private void FixedUpdate()
     {
         _ParallaxX();
+        _ParallaxY();
     }
     private void _ParallaxX()
     {
@@ -42,6 +47,22 @@ public class BackGround : MonoBehaviour
         else if (temp < _startPosX - _length)
         {
             _startPosX -= _length;
+        }
+    }
+    private void _ParallaxY()
+    {
+        float temp = _camera.transform.position.y * (1 - _parallaxEffect);
+        float dist = _camera.transform.position.y * _parallaxEffect;
+
+        transform.position = new Vector3(transform.position.x, _startPosY + dist, transform.position.z);
+
+        if (temp > _startPosY + _lengthY)
+        {
+            _startPosY += _lengthY;
+        }
+        else if (temp < _startPosY - _lengthY)
+        {
+            _startPosY -= _lengthY;
         }
     }
 }
